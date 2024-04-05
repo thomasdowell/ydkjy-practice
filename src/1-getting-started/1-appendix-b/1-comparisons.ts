5548e8664fc0c2c4a8926def6b35ae9fcfb69b97
Thomas Dowell <thomas.dowell@outlook.com>
2024-04-05 11:57:02 +0100

    Comparisons solution

--- src/1-getting-started/1-appendix-b/1-comparisons.ts
+++ src/1-getting-started/1-appendix-b/1-comparisons.ts
@@ -8,6 +8,36 @@ const dayEnd = "17:45";
  * @param durationMinutes number of minutes
  */
 export function scheduleMeeting(startTime: string, durationMinutes: number): boolean {
-  // ..TODO..
+  const startTimeMinutes = timeInMinutes(startTime);
+  const endTimeMinutes = startTimeMinutes + durationMinutes;
+
+  const dayStartMinutes = timeInMinutes(dayStart);
+  const dayEndMinutes = timeInMinutes(dayEnd);
+
+  return startTimeMinutes >= dayStartMinutes && endTimeMinutes <= dayEndMinutes;
+}
+
+/**
+ * Returns the number of minutes since midnight for a given time.
+ * @param time 24-hour format as a string "hh:mm"
+ */
+function timeInMinutes(time: string): number {
+  const [hour, minute] = parseTime(time);
+
+  return hour * 60 + minute;
+}
+
+/**
+ * Returns the hour and minute components of a time string.
+ * @param time 24-hour format as a string "hh:mm"
+ */
+function parseTime(time: string): [number, number] {
+  const [hour, minute] = time.split(":").map(Number);
+
+  if(hour === undefined || minute === undefined || isNaN(hour) || isNaN(minute) || hour < 0 || hour > 23 || minute < 0 || minute > 59) {
+    throw new Error("Invalid start time. Must be in 24-hour format as a string \"hh:mm\"");
+  }
+
+  return [hour, minute];
 }
 
